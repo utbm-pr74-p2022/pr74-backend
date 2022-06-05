f39074eceab7c9bee818f6d77bfbba705d646562
Lucas <lucas.chateau@utbm.fr>
2022-06-05 16:45:27 +0200

    Initial commit

---
+++ src/test/java/fr/utbm/pr74/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.utbm.pr74.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
