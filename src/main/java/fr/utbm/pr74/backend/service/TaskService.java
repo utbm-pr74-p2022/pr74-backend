c81eed04d694005bd3c5eee35750513c7e3fb5c8
Lucas <lucas.chateau@utbm.fr>
2022-06-23 10:53:47 +0200

    Create task with default status

--- src/main/java/fr/utbm/pr74/backend/service/TaskService.java
+++ src/main/java/fr/utbm/pr74/backend/service/TaskService.java
@@ -13,16 +13,26 @@ public class TaskService extends AbstractService<Task, TaskRepository> {
     }
 
     @Override
+    public Task create(Task entity) {
+        prepareTask(entity);
+        return super.create(entity);
+    }
+
+    @Override
     public Task update(Integer id, Task entity) {
-        if (entity.getSprint() != null) {
-            if (entity.getStatus() == null) {
-                entity.setStatus(entity.getSprint().getProject().getStatuses().get(0));
+        prepareTask(entity);
+        return super.update(id, entity);
+    }
+
+    private void prepareTask(Task task) {
+        if (task.getSprint() != null) {
+            if (task.getStatus() == null) {
+                task.setStatus(task.getSprint().getProject().getStatuses().get(0));
             }
-        } else if (entity.getBacklog() != null) {
-            entity.setStatus(null);
+        } else if (task.getBacklog() != null) {
+            task.setStatus(null);
         } else {
             throw new RuntimeException("Impossible to update task");
         }
-        return super.update(id, entity);
     }
 }
